8dc41bee6204436b5d960586067c0e75b8b212c7
eugene afanasovich <eugeneafanasovich@gmail.com>
2016-02-04 20:24:59 +0300

    какая-то хуйня (home)

--- podsobniki/podsobniki/js/default.js
+++ podsobniki/podsobniki/js/default.js
@@ -1,4 +1,5 @@
-﻿(function () {
+﻿(function ()
+{
     "use strict";
     var app = WinJS.Application;
 
@@ -12,3 +13,32 @@
 
     app.start();
 })();
+
+var mainApp = angular.module('mainApp', ['ngRoute']);
+mainApp.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: '../html/main_page.html',
+        controller: 'mainController'
+    })
+    .when('/enter_as', {
+        templateUrl: '../html/enter_page.html',
+        controller: 'enterController'
+    })
+    .when('/enter_as_customer', {
+        templateUrl: '../html/enter_customer_page.html',
+        controller: 'enterCustomerController'
+    })
+    .when('/enter_as_performer', {
+        templateUrl: '../html/enter_performer_page.html',
+        controller: 'enterPerformerController'
+    });
+});
+mainApp.controller('mainController', function ($scope) {
+});
+mainApp.controller('enterController', function ($scope) {
+});
+mainApp.controller('enterCustomerController', function ($scope) {
+});
+mainApp.controller('enterPerformerController', function ($scope) {
+});
